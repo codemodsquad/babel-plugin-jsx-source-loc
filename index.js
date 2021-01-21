8b80710c6e91f9bd100c0b597abe4cffa80c53f9
Andy Edwards <jedwards@fastmail.com>
2021-01-21 14:42:22 -0600

    fix: don't add data-source-loc to React.Fragments

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const t = require('@babel/types')
 const Path = require('path')
 
+function isReactImportOrRequire(path) {
+  if (path.parentPath.type === 'ImportDeclaration')
+    return path.parentPath.node.source.value === 'react'
+  else if (path.node.type === 'VariableDeclarator') {
+    const { init } = path.node
+    return (
+      init.type === 'CallExpression' &&
+      init.callee.type === 'Identifier' &&
+      init.callee.name === 'require' &&
+      init.arguments[0] &&
+      init.arguments[0].type === 'StringLiteral' &&
+      init.arguments[0].value === 'react'
+    )
+  }
+}
+
+function isReactFragment(path) {
+  const { node } = path
+  if (
+    node.type === 'JSXMemberExpression' &&
+    node.property.type === 'JSXIdentifier' &&
+    node.property.name === 'Fragment' &&
+    node.object.type === 'JSXIdentifier'
+  ) {
+    const binding = path.scope.getBinding(node.object.name)
+    const boundPath = binding ? binding.path : null
+    if (!boundPath) return node.object.name === 'React'
+    return isReactImportOrRequire(boundPath)
+  } else if (node.type === 'JSXIdentifier' && node.name === 'Fragment') {
+    const binding = path.scope.getBinding(node.name)
+    const boundPath = binding ? binding.path : null
+    if (!boundPath) return false
+    return isReactImportOrRequire(boundPath)
+  }
+}
+
 module.exports = function(api) {
   api.assertVersion(7)
 
@@ -10,6 +46,8 @@ module.exports = function(api) {
     name: 'jsx-source-loc',
     visitor: {
       JSXOpeningElement: (path, state) => {
+        if (isReactFragment(path.get('name'))) return
+
         const location = path.container.openingElement.loc
         if (!location) {
           // the element was generated and doesn't have location information
--- test/index.js
+++ test/index.js
@@ -48,3 +48,218 @@ it('works', async () => {
     `)
   )
 })
+
+it(`ignores React.Fragment from import * as React from 'react'`, async () => {
+  const input = `
+    import * as React from 'react'
+    function MyComponent() {
+      return (
+        <React.Fragment>
+          <Alert variant="danger">
+            {names.map(name =>
+              <Card key={name}>
+                {name}
+              </Card>
+            )}
+          </Alert>
+        </React.Fragment>
+      )
+    } 
+  `
+
+  const transformed = (await babel.transformAsync(input, {
+    plugins: ['@babel/plugin-syntax-jsx', plugin],
+    filename: 'test/index.js',
+  })).code
+
+  expect(format(transformed)).to.equal(
+    format(`
+      import * as React from 'react'
+      function MyComponent() {
+        return (
+          <React.Fragment>
+            <Alert variant="danger" data-source-loc="test/index.js:6:10">
+              {names.map(name =>
+                <Card key={name} data-source-loc="test/index.js:8:14">
+                  {name}
+                </Card>
+              )}
+            </Alert>
+          </React.Fragment>
+        )
+      } 
+    `)
+  )
+})
+
+it(`ignores React.Fragment from import React from 'react'`, async () => {
+  const input = `
+    import React from 'react'
+    function MyComponent() {
+      return (
+        <React.Fragment>
+          <Alert variant="danger">
+            {names.map(name =>
+              <Card key={name}>
+                {name}
+              </Card>
+            )}
+          </Alert>
+        </React.Fragment>
+      )
+    } 
+  `
+
+  const transformed = (await babel.transformAsync(input, {
+    plugins: ['@babel/plugin-syntax-jsx', plugin],
+    filename: 'test/index.js',
+  })).code
+
+  expect(format(transformed)).to.equal(
+    format(`
+      import React from 'react'
+      function MyComponent() {
+        return (
+          <React.Fragment>
+            <Alert variant="danger" data-source-loc="test/index.js:6:10">
+              {names.map(name =>
+                <Card key={name} data-source-loc="test/index.js:8:14">
+                  {name}
+                </Card>
+              )}
+            </Alert>
+          </React.Fragment>
+        )
+      } 
+    `)
+  )
+})
+
+it(`ignores Fragment from import { Fragment } from 'react'`, async () => {
+  const input = `
+    import React, {Fragment} from 'react'
+    function MyComponent() {
+      return (
+        <Fragment>
+          <Alert variant="danger">
+            {names.map(name =>
+              <Card key={name}>
+                {name}
+              </Card>
+            )}
+          </Alert>
+        </Fragment>
+      )
+    } 
+  `
+
+  const transformed = (await babel.transformAsync(input, {
+    plugins: ['@babel/plugin-syntax-jsx', plugin],
+    filename: 'test/index.js',
+  })).code
+
+  expect(format(transformed)).to.equal(
+    format(`
+      import React, {Fragment} from 'react'
+      function MyComponent() {
+        return (
+          <Fragment>
+            <Alert variant="danger" data-source-loc="test/index.js:6:10">
+              {names.map(name =>
+                <Card key={name} data-source-loc="test/index.js:8:14">
+                  {name}
+                </Card>
+              )}
+            </Alert>
+          </Fragment>
+        )
+      } 
+    `)
+  )
+})
+
+it(`ignores React.Fragment from const React = require('react')`, async () => {
+  const input = `
+    const React = require('react')
+    function MyComponent() {
+      return (
+        <React.Fragment>
+          <Alert variant="danger">
+            {names.map(name =>
+              <Card key={name}>
+                {name}
+              </Card>
+            )}
+          </Alert>
+        </React.Fragment>
+      )
+    } 
+  `
+
+  const transformed = (await babel.transformAsync(input, {
+    plugins: ['@babel/plugin-syntax-jsx', plugin],
+    filename: 'test/index.js',
+  })).code
+
+  expect(format(transformed)).to.equal(
+    format(`
+      const React = require('react')
+      function MyComponent() {
+        return (
+          <React.Fragment>
+            <Alert variant="danger" data-source-loc="test/index.js:6:10">
+              {names.map(name =>
+                <Card key={name} data-source-loc="test/index.js:8:14">
+                  {name}
+                </Card>
+              )}
+            </Alert>
+          </React.Fragment>
+        )
+      } 
+    `)
+  )
+})
+
+it(`ignores Fragment from const {Fragment} = require('react')`, async () => {
+  const input = `
+    const {Fragment} = require('react')
+    function MyComponent() {
+      return (
+        <Fragment>
+          <Alert variant="danger">
+            {names.map(name =>
+              <Card key={name}>
+                {name}
+              </Card>
+            )}
+          </Alert>
+        </Fragment>
+      )
+    } 
+  `
+
+  const transformed = (await babel.transformAsync(input, {
+    plugins: ['@babel/plugin-syntax-jsx', plugin],
+    filename: 'test/index.js',
+  })).code
+
+  expect(format(transformed)).to.equal(
+    format(`
+      const {Fragment} = require('react')
+      function MyComponent() {
+        return (
+          <Fragment>
+            <Alert variant="danger" data-source-loc="test/index.js:6:10">
+              {names.map(name =>
+                <Card key={name} data-source-loc="test/index.js:8:14">
+                  {name}
+                </Card>
+              )}
+            </Alert>
+          </Fragment>
+        )
+      } 
+    `)
+  )
+})
